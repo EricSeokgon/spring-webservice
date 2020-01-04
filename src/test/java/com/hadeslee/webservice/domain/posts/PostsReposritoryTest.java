f454c6584c0a1f56cc8e4ee87a18e42796b7d073
hadesyi <leesk55@gmail.com>
2020-01-05 08:25:23 +0900

    PostsReposritory 테스트 코드 작성하기

---
+++ src/test/java/com/hadeslee/webservice/domain/posts/PostsReposritoryTest.java
@@ -0,0 +1,48 @@
+package com.hadeslee.webservice.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PostsReposritoryTest {
+
+    @Autowired
+    PostsReposritory postsReposritory;
+
+    @After
+    public void cleanup() {
+        /**
+         * 이후 테스트 코드에 영향을 끼치지 않기 위해
+         * 테스트 메소드가 끝날때 마다 repository 전체 비우는 코드
+         */
+        postsReposritory.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        postsReposritory.save(Posts.builder()
+                .title("테스트 title")
+                .content("테스트 content")
+                .author("leesk55@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsReposritory.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 title"));
+        assertThat(posts.getContent(), is("테스트 content"));
+    }
+}
